2df59709374002f519ed90f6d7b9eb9afde349e0
NLSanyu <lydiansanyu@gmail.com>
2021-04-08 14:35:41 +0300

    Test ODK API authentication

--- test_script.py
+++ test_script.py
@@ -1,10 +1,26 @@
+import json
 import requests
 from decouple import config
 
+email = config('ODK_EMAIL'),
+password = config('ODK_PASSWORD')
+
 url = 'https://sunbirdainoise.mooo.com/v1/sessions'
-credentials = {
-    'email': config('ODK_EMAIL'),
-    'password': config('ODK_PASSWORD')
-}
-response = requests.get(url, auth=requests.auth.HTTPBasicAuth(credentials['email'], credentials['password']))
-print(response)
+credentials = {'email': email, 'password': password}
+headers = {'Content-Type': 'application/json'}
+
+def authenticate():
+    response = requests.post(
+        url=url,
+        # auth=requests.auth.HTTPBasicAuth(email, password),
+        json=credentials,
+        headers=headers
+    )
+    print(email)
+    print(password)
+    print(credentials)
+
+    print(response.text)
+
+if __name__=='__main__':
+    authenticate()
