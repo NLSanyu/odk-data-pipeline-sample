a0ea6d14d55c0e1f1e55f4f304f3aafdc28f0cb9
NLSanyu <lydiansanyu@gmail.com>
2021-05-26 10:51:23 +0300

    Update data pipeline

--- dags/noise_data.py
+++ dags/noise_data.py
@@ -2,17 +2,24 @@ from airflow.models import DAG
 from airflow.utils.dates import days_ago
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
+from decouple import config
+from scripts.odk_data_download import get_submissions
+from scripts.s3_data_upload import upload_to_s3
 
 
 def download_odk_media():
-    print("Download media")
+    get_submissions()
 
 def upload_media_to_s3():
-    print("Upload media")
+    upload_to_s3('audio')
 
 default_args = {
     'owner': 'Lydia',
-    'start_date': datetime(2021, 3, 1),
+    'start_date': datetime(2021, 5, 7),
+    'email': config('ODK_EMAIL'),
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 1,
     'retry_delay': timedelta(minutes=20)
 }
 
@@ -23,15 +30,13 @@ data_transfer_dag = DAG(
 )
 
 download_odk_media_task = PythonOperator(
-    task_id='download_odk_media_task',
+    task_id='download_odk_media',
     python_callable=download_odk_media,
     dag=data_transfer_dag
 )
 
 upload_media_to_s3_task = PythonOperator(
-    task_id='upload_media_to_s3_task',
+    task_id='upload_media_to_s3',
     python_callable=upload_media_to_s3,
     dag=data_transfer_dag
 )
-
-
